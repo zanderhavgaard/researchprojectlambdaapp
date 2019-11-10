454ca44971301bee5b029d77122803c3e76100b4
zanderhavgaard <zander@havgaard.dk>
2019-11-10 18:11:20 +0100

    finished preliminary implementation of FeedWebView

--- FeedWebView/code/lambda_function.py
+++ FeedWebView/code/lambda_function.py
@@ -1,19 +1,81 @@
+import sys
+import jinja2
 import json
-from flask import Flask
 
-# def lambda_handler(event, context):
-    # TODO implement
-    # return {
-    #     'statusCode': 200,
-    #     'body': json.dumps('Hello from Lambda!')
-    # }
-    app.run()
 
-app = Flask(__name__)
+html_template = """
+<html>
+  <head>
+    <title>feed_web_view</title>
+  </head>
+  <body>
+    <h1>hello world</h1>
+    {% for file_name, file_path in content.items() %}
+      <h2>{{ file_name }}</h2>
+      <img src="{{ file_path }}">
+    {% endfor %}
+  </body>
+</html>
+"""
 
-@app.route('/')
-def hello_world():
-    return "hello world"
+def lambda_handler(event, context):
 
-if __name__ == '__main__':
-    app.run()
+    content_dict = parse_feed_json(event)
+
+    html = fill_html_template(html_template, content_dict)
+
+    if local_test:
+        print(build_return(html))
+    else:
+        return build_return(html)
+
+
+def parse_feed_json(feed_json):
+    if local_test: feed_json_dict = json.loads(feed_json)
+    else: feed_json_dict = feed_json
+    feed_dict = {}
+    if feed_json_dict["StatusCode"] == 200:
+        for file_name, file_path in feed_json_dict["feed"].items():
+            feed_dict[file_name] = file_path
+    return feed_dict
+
+
+def fill_html_template(template, content_dict):
+    html_template = jinja2.Template(template)
+    return html_template.render(content=content_dict)
+
+
+def build_return(html):
+    return {
+        "StatusCode": 200,
+        "body": html,
+        "headers": {
+            "Content-Type": "text/html",
+        }
+    }
+
+
+# >>>>> testing <<<<<
+
+local_test = bool(sys.argv[1]) if len(sys.argv) > 1 else False
+
+if local_test:
+    test_dict = {
+        "file_one.png": "https://via.placeholder.com/150",
+        "file_two.png": "https://via.placeholder.com/250",
+        "file_thre.png": "https://via.placeholder.com/350"
+    }
+    test_json = """
+{
+    "StatusCode": 200,
+    "feed": {
+        "file_one.png": "https://via.placeholder.com/150",
+        "file_two.png": "https://via.placeholder.com/250",
+        "file_thre.png": "https://via.placeholder.com/350"
+    }
+}
+"""
+    # test_event = json.dumps(test_json)
+    test_event = test_json
+    test_context = None
+    lambda_handler(test_event, test_context)
