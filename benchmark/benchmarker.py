e6b6c504b44d12d078663b687dff48731818b823
zanderhavgaard <zander@havgaard.dk>
2019-12-01 00:15:51 +0100

    finished benchmark wrapper methods

--- benchmark/benchmarker.py
+++ benchmark/benchmarker.py
@@ -5,6 +5,7 @@ import time
 import json
 import requests
 
+# aux class for handling timing of things
 class Timer:
 
     def __init__(self):
@@ -26,10 +27,6 @@ class Benchmarker:
         self.url_prefix = self.api_gateway + '/live'
         self.auth_header = {'x-api-key':self.api_key, 'Content-Type':'application/json'}
 
-    def benchmark(self):
-        print('api_key:', self.api_key)
-        print('api_gateway:', self.api_gateway)
-
     def request_getter(self, command, filename=''):
         url = self.url_prefix + '/getter'
         data = {'StatusCode': 200, 'command': command}
@@ -42,7 +39,55 @@ class Benchmarker:
         # TODO @Thomas
         raise NotImplementedError()
 
+    def request_feed_generator(self, num_items='all'):
+       url = self.url_prefix + '/feed-generator'
+       data = {'StatusCode':200, 'num_items':num_items}
+       response = requests.get(url=url, data=json.dumps(data), headers=self.auth_header)
+       return response
+
+    def request_feed_webview(self, num_items='all'):
+        url = self.url_prefix + '/feed'
+        data = {'StatusCode':200, 'num_items':num_items}
+        response = requests.get(url=url, data=json.dumps(data), headers=self.auth_header)
+        return response
+
+    def test_methods(self):
+
+        print('\n\tTesting Getter\n')
+        getter_list = self.request_getter(command='list')
+        print('getter - list', getter_list.text)
+        getter_file_url = self.request_getter(command='get_file_url',filename='green.png')
+        print('getter - file_url', getter_file_url.text)
+
+        # test putter?
+        # TODO ?
+
+        print('\n\tTesting FeedGenerator\n')
+        feed_generator_all = self.request_feed_generator()
+        print('feed_generator - all', feed_generator_all.text)
+        feed_generator_2 = self.request_feed_generator(num_items=2)
+        print('feed_generator - 2', feed_generator_2.text)
+
+        print('\n\tTesting FeedWebView\n')
+        feed_all = self.request_feed_webview()
+        print('feed - all', feed_all.text)
+        feed_2 = self.request_feed_webview(num_items=2)
+        print('feed - 2', feed_2.text)
+
+
+    def benchmark(self):
+        print('api_key:', self.api_key)
+        print('api_gateway:', self.api_gateway)
+
+
+
+# ==============================
+# >>>>> run the benchmarks <<<<<
+
 benchmarker = Benchmarker()
-# benchmarker.benchmark()
-print(benchmarker.request_getter(command='list').text)
-print(benchmarker.request_getter(command='get_file_url',filename='green.png').text)
+
+# test that methods work...
+# benchmarker.test_methods()
+
+# run benchmarks!
+benchmarker.benchmark()
