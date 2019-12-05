bf94a3af96ed7d062254eee572a4aa911c622c5a
Thomas <thomas@localhost.localdomain>
2019-12-05 17:52:05 +0100

    (hopefully) last fuck up

--- Experiments.py/run_max_warm_test.py
+++ Experiments.py/run_max_warm_test.py
@@ -1,4 +1,4 @@
 from max_warm_test import max_warm_test
 import sys
 
-max_warm_test(sys.argv[1],int(sys.argv[2]),float(sys.argv[3]),float(sys.argv[4]))
+max_warm_test(sys.argv[1],sys.argv[2],int(sys.argv[3]),float(sys.argv[4]),float(sys.argv[5]))
